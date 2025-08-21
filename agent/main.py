f99f842e5ab295f4dc7bf21774cd2b787b9c92b3
Vyacheslav Kapitonov <vyach.kapitonov@gmail.com>
2025-08-21 13:23:17 +0300

    Delete NamedTuple

--- agent/main.py
+++ agent/main.py
@@ -23,6 +23,7 @@ from langchain_core.messages import SystemMessage
 
 from agent import ai
 from agent import utils
+from agent.prompts import SYSTEM_PROMPT
 from agent.todo import HERE
 from agent.todo import storage
 from agent.todo import Task
@@ -128,7 +129,6 @@ async def voice_command_handler(message: Message) -> None:
     responses.append(await _msg(text='Отправляю файл на транскрибацию'))
     transcription = ai.convert_audio_to_text(output_file.as_posix())
     responses.append(await _msg(text='Запускаю агента'))
-    from agent.prompts import SYSTEM_PROMPT
     for s in ai.agent.stream(
             {
                 'messages':
